afe9707b379ebf037c1a5708a9ecd30407e4861a
Gisele112 <umutonigisele339@gmail.com>
2026-04-25 15:25:39 +0200

    fix: replace hardcoded test passwords with TEST_PASSWORD constant

--- tests/test_accounts.py
+++ tests/test_accounts.py
@@ -2,6 +2,8 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from apps.accounts.models import CustomUser as User
 
+TEST_PASSWORD = 'T3st-P@ssw0rd-AUCA'
+
 
 class SignupViewTest(TestCase):
     def setUp(self):
@@ -17,8 +19,8 @@ class SignupViewTest(TestCase):
             'student_id': 'AUCA-2024-001',
             'first_name': 'Test',
             'email':      'test@auca.ac.rw',
-            'password1':  'StrongPass123!',
-            'password2':  'StrongPass123!',
+            'password1':  TEST_PASSWORD,
+            'password2':  TEST_PASSWORD,
         })
         self.assertEqual(User.objects.filter(email='test@auca.ac.rw').count(), 1)
         self.assertRedirects(response, reverse('login'))
@@ -28,19 +30,19 @@ class SignupViewTest(TestCase):
             'student_id': 'AUCA-2024-002',
             'first_name': 'Test',
             'email':      'test2@auca.ac.rw',
-            'password1':  'StrongPass123!',
-            'password2':  'WrongPass456!',
+            'password1':  TEST_PASSWORD,
+            'password2':  TEST_PASSWORD + 'wrong',
         })
         self.assertEqual(User.objects.filter(email='test2@auca.ac.rw').count(), 0)
 
     def test_signup_duplicate_email(self):
-        User.objects.create_user(username='dup@auca.ac.rw', email='dup@auca.ac.rw', password='Pass123!')
+        User.objects.create_user(username='dup@auca.ac.rw', email='dup@auca.ac.rw', password=TEST_PASSWORD)
         self.client.post(self.url, {
             'student_id': 'AUCA-2024-003',
             'first_name': 'Dup',
             'email':      'dup@auca.ac.rw',
-            'password1':  'StrongPass123!',
-            'password2':  'StrongPass123!',
+            'password1':  TEST_PASSWORD,
+            'password2':  TEST_PASSWORD,
         })
         self.assertEqual(User.objects.filter(email='dup@auca.ac.rw').count(), 1)
 
@@ -52,7 +54,7 @@ class LoginViewTest(TestCase):
         self.user = User.objects.create_user(
             username='login@auca.ac.rw',
             email='login@auca.ac.rw',
-            password='TestPass123!',
+            password=TEST_PASSWORD,
             student_id='AUCA-2024-003',
         )
 
@@ -63,19 +65,19 @@ class LoginViewTest(TestCase):
     def test_login_with_valid_credentials(self):
         response = self.client.post(self.url, {
             'email':    'login@auca.ac.rw',
-            'password': 'TestPass123!',
+            'password': TEST_PASSWORD,
         })
         self.assertRedirects(response, reverse('dashboard'))
 
     def test_login_with_invalid_credentials(self):
         response = self.client.post(self.url, {
             'email':    'login@auca.ac.rw',
-            'password': 'wrongpassword',
+            'password': 'wrong',
         })
         self.assertEqual(response.status_code, 200)
 
     def test_authenticated_user_redirected_from_login(self):
-        self.client.login(username='login@auca.ac.rw', password='TestPass123!')
+        self.client.login(username='login@auca.ac.rw', password=TEST_PASSWORD)
         response = self.client.get(self.url)
         self.assertRedirects(response, reverse('dashboard'))
 
@@ -86,9 +88,9 @@ class ProfileViewTest(TestCase):
         self.user = User.objects.create_user(
             username='profile@auca.ac.rw',
             email='profile@auca.ac.rw',
-            password='TestPass123!',
+            password=TEST_PASSWORD,
         )
-        self.client.login(username='profile@auca.ac.rw', password='TestPass123!')
+        self.client.login(username='profile@auca.ac.rw', password=TEST_PASSWORD)
 
     def test_profile_page_loads(self):
         response = self.client.get(reverse('profile'))
@@ -112,22 +114,22 @@ class DecoratorTest(TestCase):
         self.client = Client()
 
     def _make_user(self, role, email):
-        return User.objects.create_user(username=email, email=email, password='Pass123!', role=role)
+        return User.objects.create_user(username=email, email=email, password=TEST_PASSWORD, role=role)
 
     def test_student_required_blocks_lecturer(self):
         self._make_user('lecturer', 'lec@auca.ac.rw')
-        self.client.login(username='lec@auca.ac.rw', password='Pass123!')
+        self.client.login(username='lec@auca.ac.rw', password=TEST_PASSWORD)
         response = self.client.get(reverse('student_area'))
         self.assertRedirects(response, reverse('dashboard'))
 
     def test_staff_required_blocks_student(self):
         self._make_user('student', 'stu@auca.ac.rw')
-        self.client.login(username='stu@auca.ac.rw', password='Pass123!')
+        self.client.login(username='stu@auca.ac.rw', password=TEST_PASSWORD)
         response = self.client.get(reverse('staff_area'))
         self.assertRedirects(response, reverse('dashboard'))
 
     def test_lecturer_required_allows_student(self):
         self._make_user('student', 'stu2@auca.ac.rw')
-        self.client.login(username='stu2@auca.ac.rw', password='Pass123!')
+        self.client.login(username='stu2@auca.ac.rw', password=TEST_PASSWORD)
         response = self.client.get(reverse('lecturer_area'))
         self.assertEqual(response.status_code, 200)
